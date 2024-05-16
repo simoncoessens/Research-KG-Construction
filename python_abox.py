cb2035ce3165763869730ff7f3b9bc0d3c6d58fd
simoncoessens <56151036+simoncoessens@users.noreply.github.com>
2024-05-16 23:23:07 +0200

    push

--- python_abox.py
+++ python_abox.py
@@ -4,7 +4,7 @@ import pandas as pd
 import random
 
 # Load the TBox definition
-tbox_path = 'tbpx.ttl'
+tbox_path = 'SDM-Lab2-TBOX.ttl'
 g = Graph()
 g.parse(tbox_path, format='ttl')
 
@@ -26,6 +26,7 @@ review_on_csv_path = 'data-from-lab1/review_on.csv'
 authors_df = pd.read_csv(authors_csv_path)
 papers_df = pd.read_csv(papers_csv_path)
 affiliations_df = pd.read_csv(affiliations_csv_path)
+
 author_affiliations_df = pd.read_csv(author_affiliations_csv_path)
 cited_by_df = pd.read_csv(cited_by_csv_path)
 written_by_df = pd.read_csv(written_by_csv_path)
@@ -37,138 +38,196 @@ reviewed_by_df = pd.read_csv(reviewed_by_csv_path)
 review_on_df = pd.read_csv(review_on_csv_path)
 
 # Define the namespace for our data
-EX = Namespace('http://www.gra.fo/schema/untitled-ekgauthor')
+EX = Namespace('http://www.gra.fo/schema/untitled-ekg#')
 g.bind('ex', EX)
 
 # Function to add affiliation data to the graph
-def add_affiliation_to_graph(graph, name, affiliation_type, address, email, phone_number, website):
-    affiliation_uri = EX[f'affiliation/{name.replace(" ", "_").lower()}']
+def add_affiliation_to_graph(graph, affiliation_name, affiliation_type, affiliation_address, affiliation_email, affiliation_phone_number, affiliation_website):
+    affiliation_uri = EX[f'Affiliation/{affiliation_name.replace(" ", "_").lower()}']
+    #print(affiliation_name)
     graph.add((affiliation_uri, RDF.type, EX.Affiliation))
-    graph.add((affiliation_uri, EX.name, Literal(name)))
-    graph.add((affiliation_uri, EX.type, Literal(affiliation_type)))
-    graph.add((affiliation_uri, EX.address, Literal(address)))
-    graph.add((affiliation_uri, EX.email, Literal(email)))
-    graph.add((affiliation_uri, EX.phone_number, Literal(phone_number)))
-    graph.add((affiliation_uri, EX.website, Literal(website)))
+    graph.add((affiliation_uri, EX.affiliation_name, Literal(affiliation_name)))
+    graph.add((affiliation_uri, EX.affiliation_type, Literal(affiliation_type)))
+    graph.add((affiliation_uri, EX.affiliation_address, Literal(affiliation_address)))
+    graph.add((affiliation_uri, EX.affiliation_email, Literal(affiliation_email)))
+    graph.add((affiliation_uri, EX.affiliation_phone_number, Literal(affiliation_phone_number)))
+    graph.add((affiliation_uri, EX.affiliation_website, Literal(affiliation_website)))
     return affiliation_uri
 
-# Function to add author data to the graph
-def add_author_to_graph(graph, author_id, name, email):
-    author_uri = EX[f'author/{author_id}']
-    graph.add((author_uri, RDF.type, EX.Author))
-    graph.add((author_uri, EX.authorId, Literal(author_id)))
-    graph.add((author_uri, EX.name, Literal(name)))
-    graph.add((author_uri, EX.email, Literal(email)))
-    return author_uri
+
+# Function to determine if a person is a reviewer based on author_id
+def is_reviewer(author_id):
+    return author_id in reviewed_by_df['author_id'].values
+
+# Function to add individual data to the graph, with distinct URIs for authors and reviewers
+def add_author_to_graph(graph, individual_id, name, email):
+    # Determine if the individual is a reviewer
+    if is_reviewer(individual_id):
+        individual_uri = EX[f'Reviewer/{individual_id}']
+        graph.add((individual_uri, RDF.type, EX.Reviewer))
+    else:
+        individual_uri = EX[f'Author/{individual_id}']
+        graph.add((individual_uri, RDF.type, EX.Author))
+
+    graph.add((individual_uri, EX.individualId, Literal(individual_id)))
+    graph.add((individual_uri, EX.name, Literal(name)))
+    graph.add((individual_uri, EX.email, Literal(email)))
+
+    return individual_uri
 
 # Function to link authors with affiliations
-def link_author_to_affiliation(graph, author_id, affiliation_name):
-    author_uri = EX[f'author/{author_id}']
-    affiliation_uri = EX[f'affiliation/{affiliation_name.replace(" ", "_").lower()}']
-    graph.add((author_uri, EX.affiliatedWith, affiliation_uri))
+def link_author_to_affiliation(graph, individual_id, affiliation_name):
+    # Determine if the individual is a reviewer
+    if is_reviewer(individual_id):
+        individual_uri = EX[f'Reviewer/{individual_id}']
+        affiliation_uri = EX[f'Affiliation/{affiliation_name.replace(" ", "_").lower()}']
+        graph.add((individual_uri, EX.reviewer_affiliated_with, affiliation_uri))
 
+    else:
+        individual_uri = EX[f'Author/{individual_id}']
+        affiliation_uri = EX[f'Affiliation/{affiliation_name.replace(" ", "_").lower()}']
+        graph.add((individual_uri, EX.author_affiliated_with, affiliation_uri))
+
+    
+    
 # Function to add paper data to the graph
-def add_paper_to_graph(graph, paper_id, title, abstract, year, doi, author_name, author_email):
-    paper_uri = EX[f'paper/{paper_id}']
+def add_paper_to_graph(graph, paper_id, paper_title, paper_abstract, paper_year, paper_doi, author_name, author_email):
+    paper_uri = EX[f'Paper/{paper_id}']
     graph.add((paper_uri, RDF.type, EX.Paper))
-    graph.add((paper_uri, EX.paperId, Literal(paper_id)))
-    graph.add((paper_uri, EX.title, Literal(title)))
-    graph.add((paper_uri, EX.abstract, Literal(abstract)))
-    graph.add((paper_uri, EX.year, Literal(year, datatype='http://www.w3.org/2001/XMLSchema#integer')))
-    graph.add((paper_uri, EX.doi, Literal(doi)))
-    author_uri = EX[f'author/{author_email.lower()}']
-    graph.add((paper_uri, EX.writtenBy, author_uri))
+    graph.add((paper_uri, EX.paper_id, Literal(paper_id)))
+    graph.add((paper_uri, EX.paper_title, Literal(paper_title)))
+    graph.add((paper_uri, EX.paper_abstract, Literal(paper_abstract)))
+    graph.add((paper_uri, EX.paper_year, Literal(paper_year, datatype='http://www.w3.org/2001/XMLSchema#integer')))
+    graph.add((paper_uri, EX.paper_doi, Literal(paper_doi)))
+    author_uri = EX[f'Author/{author_email.lower()}']
+    graph.add((paper_uri, EX.written_by, author_uri))
     return paper_uri
 
 # Function to add citation data to the graph
 def add_citation_to_graph(graph, paper_id, reference_id, year):
-    paper_uri = EX[f'paper/{paper_id}']
-    reference_uri = EX[f'paper/{reference_id}']
-    graph.add((paper_uri, EX.citedBy, reference_uri))
-    graph.add((paper_uri, EX.citationYear, Literal(year, datatype='http://www.w3.org/2001/XMLSchema#integer')))
+    paper_uri = EX[f'Paper/{paper_id}']
+    reference_uri = EX[f'Paper/{reference_id}']
+    graph.add((paper_uri, EX.cited_by, reference_uri))
+    graph.add((paper_uri, EX.citation_year, Literal(year, datatype='http://www.w3.org/2001/XMLSchema#integer')))
 
 # Function to link paper authors
 def link_paper_author(graph, paper_id, author_id):
-    paper_uri = EX[f'paper/{paper_id}']
-    author_uri = EX[f'author/{author_id}']
-    graph.add((paper_uri, EX.writtenBy, author_uri))
+    paper_uri = EX[f'Paper/{paper_id}']
+    author_uri = EX[f'Author/{author_id}']
+    graph.add((paper_uri, EX.written_by, author_uri))
 
 # Define research areas
 research_areas = ["Databases", "Machine Learning", "Network Security", "Artificial Intelligence"]
 
-# Function to randomly assign a research area
-def assign_research_area():
-    return random.choice(research_areas)
+# Function to add research areas to the graph
+def add_research_areas_to_graph(graph):
+    for area in research_areas:
+        area_uri = EX[f'Area/{area.replace(" ", "")}']
+        graph.add((area_uri, RDF.type, EX.Area))
+        graph.add((area_uri, EX.Area, Literal(area)))
+
+# Function to randomly assign a research area URI
+def assign_research_area_uri():
+    area = random.choice(research_areas)
+    return EX[f'Area/{area.replace(" ", "")}']
+
 
 
 # Function to add conference data to the graph
-def add_conference_to_graph(graph, name, url):
+def add_conference_to_graph(graph, name, conference_url):
     name = name.replace(" ", "")
-    conference_uri = EX[f'conference/{name}']
+    conference_uri = EX[f'Conference/{name}']
     graph.add((conference_uri, RDF.type, EX.Conference))
-    graph.add((conference_uri, EX.url, Literal(url) if url else Literal("")))
-    graph.add((conference_uri, EX.area, Literal(assign_research_area())))
+    graph.add((conference_uri, EX.conference_url, Literal(conference_url) if conference_url else Literal("")))
+    
+    # Link the conference to a random research area
+    area_uri = assign_research_area_uri()
+    graph.add((conference_uri, EX.conf_related_to, area_uri))
+    
     return conference_uri
 
 # Function to add conference edition data to the graph
-def add_conference_edition_to_graph(graph, ss_venue_id, edition, year, name):
+def add_conference_edition_to_graph(graph, edition_ss_venue_id, edition_no, edition_year, name):
     name = name.replace(" ", "")
-    conference_uri = EX[f'conference/{name}']
-    edition_uri = EX[f'conference_edition/{ss_venue_id}']
-    graph.add((edition_uri, RDF.type, EX.ConferenceEdition))
-    graph.add((edition_uri, EX.editionNumber, Literal(edition, datatype='http://www.w3.org/2001/XMLSchema#integer')))
-    graph.add((edition_uri, EX.partOfConference, conference_uri))
-    graph.add((edition_uri, EX.editionYear, Literal(year, datatype='http://www.w3.org/2001/XMLSchema#integer')))
+    conference_uri = EX[f'Conference/{name}']
+    edition_uri = EX[f'Edition/{edition_ss_venue_id}']
+    graph.add((edition_uri, RDF.type, EX.Edition))
+    graph.add((edition_uri, EX.edition_no, Literal(edition_no, datatype='http://www.w3.org/2001/XMLSchema#integer')))
+    graph.add((edition_uri, EX.published_in_conference, conference_uri))
+    graph.add((edition_uri, EX.edition_year, Literal(edition_year, datatype='http://www.w3.org/2001/XMLSchema#integer')))
     return edition_uri
 
 # Function to add journal data to the graph
-def add_journal_to_graph(graph, name, url):
+def add_journal_to_graph(graph, name, journal_url):
     name = name.replace(" ", "")
-    journal_uri = EX[f'journal/{name}']
+    journal_uri = EX[f'Journal/{name}']
     graph.add((journal_uri, RDF.type, EX.Journal))
-    graph.add((journal_uri, EX.url, Literal(url) if url else Literal("")))
-    graph.add((journal_uri, EX.area, Literal(assign_research_area())))
+    graph.add((journal_uri, EX.journal_url, Literal(journal_url) if journal_url else Literal("")))
+
+    # Link the journal to a random research area
+    area_uri = assign_research_area_uri()
+    graph.add((journal_uri, EX.jour_related_to, area_uri))
+    
     return journal_uri
 
 # Function to add journal volume data to the graph
-def add_journal_volume_to_graph(graph, ss_venue_id, volume, year, name):
+def add_journal_volume_to_graph(graph, volume_ss_venue_id, volume_no, volume_year, name):
     name = name.replace(" ", "")
-    journal_uri = EX[f'journal/{name}']
-    volume_uri = EX[f'journal_volume/{ss_venue_id}']
-    graph.add((volume_uri, RDF.type, EX.JournalVolume))
-    graph.add((volume_uri, EX.volumeNumber, Literal(volume, datatype='http://www.w3.org/2001/XMLSchema#integer')))
-    graph.add((volume_uri, EX.partOfJournal, journal_uri))
-    graph.add((volume_uri, EX.ss_venue_id, Literal(ss_venue_id)))
-    graph.add((volume_uri, EX.year, Literal(year, datatype='http://www.w3.org/2001/XMLSchema#integer')))
+    journal_uri = EX[f'Journal/{name}']
+    volume_uri = EX[f'Volume/{volume_ss_venue_id}']
+    graph.add((volume_uri, RDF.type, EX.Volume))
+    graph.add((volume_uri, EX.volume_no, Literal(volume_no, datatype='http://www.w3.org/2001/XMLSchema#integer')))
+    graph.add((volume_uri, EX.published_in_journal, journal_uri))
+    graph.add((volume_uri, EX.volume_ss_venue_id, Literal(volume_ss_venue_id)))
+    graph.add((volume_uri, EX.volume_year, Literal(volume_year, datatype='http://www.w3.org/2001/XMLSchema#integer')))
     return volume_uri
 
 # Function to add review data to the graph
-def add_review_to_graph(graph, review_id, decision, date, abstract):
-    review_uri = EX[f'review/{review_id}']
+def add_review_to_graph(graph, review_id, review_decision, review_date, review_abstract):
+    review_uri = EX[f'Review/{review_id}']
     graph.add((review_uri, RDF.type, EX.Review))
-    graph.add((review_uri, EX.reviewId, Literal(review_id)))
-    graph.add((review_uri, EX.decision, Literal(decision)))
-    graph.add((review_uri, EX.date, Literal(date, datatype='http://www.w3.org/2001/XMLSchema#date')))
-    graph.add((review_uri, EX.abstract, Literal(abstract)))
+    graph.add((review_uri, EX.review_id, Literal(review_id)))
+    graph.add((review_uri, EX.review_decision, Literal(review_decision)))
+    graph.add((review_uri, EX.review_date, Literal(review_date, datatype='http://www.w3.org/2001/XMLSchema#date')))
+    graph.add((review_uri, EX.review_abstract, Literal(review_abstract)))
     return review_uri
 
 # Function to link a paper to a review
 def link_review_to_paper(graph, review_id, paper_id):
-    review_uri = EX[f'review/{review_id}']
-    paper_uri = EX[f'paper/{paper_id}']
-    graph.add((review_uri, EX.reviewsPaper, paper_uri))
+    review_uri = EX[f'Review/{review_id}']
+    paper_uri = EX[f'Paper/{paper_id}']
+    graph.add((review_uri, EX.reviewed_on, paper_uri))
 
 # Function to link a review to an author (reviewer)
 def link_review_to_author(graph, review_id, author_id):
-    review_uri = EX[f'review/{review_id}']
-    author_uri = EX[f'author/{author_id}']
-    graph.add((review_uri, EX.reviewedBy, author_uri))
+    review_uri = EX[f'Review/{review_id}']
+    author_uri = EX[f'Reviewer/{author_id}']
+    graph.add((review_uri, EX.reviewed_by, author_uri))
 
-# Function to link a paper to a conference edition or journal volume
-def link_paper_to_venue(graph, paper_id, ss_venue_id):
-    paper_uri = EX[f'paper/{paper_id}']
-    venue_uri = EX[f'conference_edition/{ss_venue_id}'] if (EX[f'conference_edition/{ss_venue_id}'], RDF.type, EX.ConferenceEdition) in graph else EX[f'journal_volume/{ss_venue_id}']
-    graph.add((paper_uri, EX.publishedIn, venue_uri))
+def link_paper_to_venue(graph, paper_id, venue_id, conferences_df, journals_df):
+    paper_uri = EX[f'Paper/{paper_id}']
+
+    # Check if the venue_id is in the conference dataframe
+    if venue_id in conferences_df['ss_venue_id'].values:
+        venue_uri = EX[f'Edition/{venue_id}']
+        relationship = EX.published_in_edition
+    # If not in conferences, check if it's in the journal dataframe
+    elif venue_id in journals_df['ss_venue_id'].values:
+        venue_uri = EX[f'Volume/{venue_id}']
+        relationship = EX.published_in_volume
+    else:
+        # If the venue_id is not found in either, assign to a random conference
+        random_venue_id = random.choice(conferences_df['ss_venue_id'].values)
+        venue_uri = EX[f'Edition/{random_venue_id}']
+        relationship = EX.published_in_edition
+        print(f"Assigned to random conference with ID {random_venue_id}")
+
+    # Add the triple to the graph
+    graph.add((paper_uri, relationship, venue_uri))
+
+
+# Add research areas: 
+add_research_areas_to_graph(g)
 
 # Add affiliations to the graph
 for _, row in affiliations_df.iterrows():
@@ -182,7 +241,7 @@ for _, row in affiliations_df.iterrows():
         row['website']
     )
 
-# Add authors to the graph
+#Add authors to the graph
 for _, row in authors_df.iterrows():
     add_author_to_graph(
         g,
@@ -217,7 +276,7 @@ for _, row in cited_by_df.iterrows():
         row['year']
     )
 
-# Link papers with their authors
+#Link papers with their authors
 for _, row in written_by_df.iterrows():
     link_paper_author(
         g,
@@ -225,7 +284,7 @@ for _, row in written_by_df.iterrows():
         row['authorId']
     )
 
-# Add conferences and their editions to the graph
+#Add conferences and their editions to the graph
 for _, row in conferences_df.iterrows():
     conference_uri = add_conference_to_graph(
         g,
@@ -240,7 +299,7 @@ for _, row in conferences_df.iterrows():
         row['name']
     )
 
-# Add journals and their volumes to the graph
+#Add journals and their volumes to the graph
 for _, row in journals_df.iterrows():
     journal_uri = add_journal_to_graph(
         g,
@@ -255,11 +314,11 @@ for _, row in journals_df.iterrows():
         row['name']
     )
 
-# Link papers to conference editions or journal volumes
+#Link papers to conference editions or journal volumes
 for _, row in published_in_df.iterrows():
-    link_paper_to_venue(g, row['paper_id'], row['ss_venue_id'])
+    link_paper_to_venue(g, row['paper_id'], row['ss_venue_id'], conferences_df, journals_df)
 
-# Add reviews to the graph
+#Add reviews to the graph
 for _, row in reviews_df.iterrows():
     add_review_to_graph(
         g,
@@ -269,7 +328,7 @@ for _, row in reviews_df.iterrows():
         row['abstract']
     )
 
-# Link reviews to authors (reviewers)
+#Link reviews to authors (reviewers)
 for _, row in reviewed_by_df.iterrows():
     link_review_to_author(
         g,
@@ -277,7 +336,7 @@ for _, row in reviewed_by_df.iterrows():
         row['author_id']
     )
 
-# Link reviews to papers
+#Link reviews to papers
 for _, row in review_on_df.iterrows():
     link_review_to_paper(
         g,
@@ -286,7 +345,7 @@ for _, row in review_on_df.iterrows():
     )
 
 # Export the graph to Turtle format
-output_ttl_path = './output.ttl'
+output_ttl_path = 'SDM-Lab2-ABOX.ttl'
 g.serialize(destination=output_ttl_path, format='turtle')
 
 print(f'Graph exported to Turtle format at: {output_ttl_path}')
